c814668279535f0bfb9687d05f85ec1886dabf73
t34b1 <66898612+t34b1@users.noreply.github.com>
2025-04-22 19:18:44 -0700

    fix broken paths

--- scripts/utils/utils-animations.js
+++ scripts/utils/utils-animations.js
@@ -12,44 +12,48 @@ export const animations = {
 
 };
 
-async function showNext(event) {
-  const cards = [];
-  for (let i = 1; i <= totalCards; i++) {
-    cards[i] = `../../images/wnrs/card-${i}.png`;
-  }
+function clickSlider(container, next, previous) {
+  const totalCards = 3;
+  let cardNumber = 1;
+  let newCard;
 
-  if (window.getComputedStyle(title).display !== "none") {
-    title.classList.add("hidden");
-  }
-
-  if (cardNumber <= totalCards) {
-    if (cardContainer.contains(newCard)) {
-      newCard.classList.remove("move-in");
-      newCard.classList.add("move-out");
-      await delay(200);
-      newCard.remove();
+  async function showNext(event) {
+    const cards = [];
+    for (let i = 1; i <= totalCards; i++) {
+      cards[i] = `../../images/wnrs/card-${i}.png`;
     }
-
-    newCard = document.createElement("img");
-    newCard.src = cards[cardNumber];
-    newCard.classList.add("card", "move-in");
-    cardContainer.append(newCard);
-    cardNumber++;
-  } else {
-    newCard.remove();
-    title.classList.remove("hidden");
-    cardNumber = 1;
+  
+    if (window.getComputedStyle(title).display !== "none") {
+      title.classList.add("hidden");
+    }
+  
+    if (cardNumber <= totalCards) {
+      if (cardContainer.contains(newCard)) {
+        newCard.classList.remove("move-in");
+        newCard.classList.add("move-out");
+        await delay(200);
+        newCard.remove();
+      }
+  
+      newCard = document.createElement("img");
+      newCard.src = cards[cardNumber];
+      newCard.classList.add("card", "move-in");
+      cardContainer.append(newCard);
+      cardNumber++;
+    } else {
+      newCard.remove();
+      title.classList.remove("hidden");
+      cardNumber = 1;
+    }
   }
 }
+
 
 export function exploration() {
   let title = document.querySelector("#title");
   let next = document.querySelector("#next");
   let previous = document.querySelector("#previous");
   let cardContainer = document.querySelector("#card-container");
-  const totalCards = 3;
-  let cardNumber = 1;
-  let newCard;
 
   next.addEventListener("click", showNext);
 }
